5b695011ecb03fe863ec9b3604f9ff52c821c221
Joseph Golden and Reggie Golden <reggie@>
2020-02-13 20:36:09 -0800

    Added scripts js file.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  var music = $("#music").val();
+  var food = $("input:restaurant[name=food]:checked").val();
+  var visit = $("#date").val();
+  var favoriteColor = $("#color").val();
+});
